0a83dfbb7c10b6386811ce6fdf2917bb12cf601a
DSlundro <dragan.savic95@outlook.it>
2022-03-30 10:51:46 +0200

    snack 2

---
+++ main.js
@@ -0,0 +1,22 @@
+//      SNACK 2 
+//  L’utente inserisce due parole in successione, con due prompt.
+//  Il software stampa prima la parola più corta, poi la parola più lunga.
+
+
+const parola_1 = prompt('Inserisci la prima parola')
+console.log(parola_1)
+
+const parola_2 = prompt('Inserisci la seconda parola')
+console.log(parola_2)
+
+if (parola_1.length < parola_2.length){
+    console.log('Parola più corta: ' + parola_1)
+    console.log('Parola più lunga: ' + parola_2)
+} 
+else if(parola_1.length > parola_2.length){
+    console.log('Parola più corta: ' + parola_2)
+    console.log('Parola più lunga: ' + parola_2)
+}
+else if(parola_1.length = parola_2.length){
+    console.log('La lunghezza delle parole è uguale')
+}
--- snack-1/main.js
+++ snack-1/main.js
@@ -2,8 +2,6 @@
 //  L’utente inserisce due numeri in successione, con due prompt.
 //  Il software stampa il maggiore.
 
-
-
 const numberOne = prompt('Inserisci il primo numero');
 console.log(numberOne)
 
@@ -12,12 +10,10 @@ console.log(numberTwo)
 
 
 if (numberOne > numberTwo){
-    console.log('Questo è il numero maggiore ' + numberOne)
-}  else if (numberTwo > numberOne){
-    console.log('Questo è il numero maggiore ' + numberTwo)
+    console.log('Questo è il numero maggiore ' + numberOne);
+}  else if (numberOne < numberTwo){
+    console.log('Questo è il numero maggiore ' + numberTwo);
 }  else if (numberOne == numberTwo){
-    console.log('I numeri sono pari')
+    console.log('I numeri sono pari');
 }
-
-
 
