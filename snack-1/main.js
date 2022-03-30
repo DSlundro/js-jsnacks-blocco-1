8052501f224f0c0c61f1ba4943b7b0f0a033d6cc
DSlundro <dragan.savic95@outlook.it>
2022-03-30 13:36:41 +0200

    snack 1 completed

--- snack-1/main.js
+++ snack-1/main.js
@@ -1,19 +1,23 @@
 //      SNACK 1 
-//  L’utente inserisce due numeri in successione, con due prompt.
-//  Il software stampa il maggiore.
 
-const numberOne = prompt('Inserisci il primo numero');
+//  L’utente inserisce due numeri in successione, con due prompt.
+const numberOne = Number(prompt('Inserisci il primo numero'));
 console.log(numberOne)
 
-const numberTwo = prompt('Inserisci il secondo numero');
+const numberTwo = Number(prompt('Inserisci il secondo numero'));
 console.log(numberTwo)
 
 
+// se non sono numeri stampa un messaggio di errore
+if(isNaN(numberOne) && isNaN(numberTwo)){
+    console.log('ERRORE: Inserisci un numero!');
+}
+//  Il software stampa il maggiore.
 if (numberOne > numberTwo){
     console.log('Questo è il numero maggiore ' + numberOne);
 }  else if (numberOne < numberTwo){
     console.log('Questo è il numero maggiore ' + numberTwo);
 }  else if (numberOne == numberTwo){
     console.log('I numeri sono pari');
-}
+}  else {console.log}
 
