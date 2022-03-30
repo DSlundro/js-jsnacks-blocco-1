335a67774815ce26f85b3eae80250f279d444118
DSlundro <dragan.savic95@outlook.it>
2022-03-30 16:19:39 +0200

    snack 7

---
+++ snack-7 - done/main.js
@@ -0,0 +1,22 @@
+//      SNACK 7
+
+//  stampa le potenze di 2 fino a 1000
+
+// imposto il numero
+let numero;
+
+// imposto l'esponente
+let potenza = 2;
+
+/* // numero elevato alla potenza
+for (let i = 0; i < 1000; i++) {
+    numero = i
+    console.log(Math.pow(numero, potenza));
+}
+ */
+
+// la potenza elevata al numero
+for (let i = 0; i < 1000; i++) {
+    numero = i
+    console.log(Math.pow(potenza, numero));
+}
