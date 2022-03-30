1ddc49d11063f63b541291d255c0d2d1c94e5a24
DSlundro <dragan.savic95@outlook.it>
2022-03-30 12:53:08 +0200

    update

--- snack-5/main.js
+++ snack-5/main.js
@@ -1,21 +1,20 @@
 //      SNACK 5
+
 //  Crea un array vuoto.
-//  Chiedi per 6 volte all’utente di inserire un numero,
-//  se è dispari inseriscilo nell’array.
-
-
-
 const vuoto = []
 
+//  Chiedi per 6 volte all’utente di inserire un numero,
 for(let i = 0; i < 6; i++){
 
     let numero = parseInt(prompt('Inserisci un numero'));
 
+    // controllo se il dato inserito è un numero
     if(isNaN(numero)){
         console.log('ERRORE: Inserisci un numero!');
         break;
     }
 
+    //  se è dispari inseriscilo nell’array.
     if (numero % 2 != 0){
         vuoto.push(numero);
         console.log(numero)
