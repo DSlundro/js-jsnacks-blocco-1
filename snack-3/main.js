72bd2c79323eb9b292f3914388e30b7e8d28795f
DSlundro <dragan.savic95@outlook.it>
2022-03-30 11:40:07 +0200

    update

--- snack-3/main.js
+++ snack-3/main.js
@@ -4,35 +4,35 @@
 
 
 const numero_1 = Number(prompt('Intesisci il primo numero'));
-console.log(numero_1);
+console.log('Numero 1: '+ numero_1);
 
 const numero_2 = Number(prompt('Intesisci il secondo numero'));
-console.log(numero_2);
+console.log('Numero 2: '+ numero_2);
 
 const numero_3 = Number(prompt('Intesisci il terzo numero'));
-console.log(numero_3);
+console.log('Numero 3: '+ numero_3);
 
 const numero_4 = Number(prompt('Intesisci il quarto numero'));
-console.log(numero_4);
+console.log('Numero 4: '+ numero_4);
 
 const numero_5 = Number(prompt('Intesisci il quinto numero'));
-console.log(numero_5);
+console.log('Numero 5: '+ numero_5);
 
 const numero_6 = Number(prompt('Intesisci il sesto numero'));
-console.log(numero_6);
+console.log('Numero 6: '+ numero_6);
 
 const numero_7 = Number(prompt('Intesisci il settimo numero'));
-console.log(numero_7);
+console.log('Numero 7: '+ numero_7);
 
 const numero_8 = Number(prompt('Intesisci l\'ottavo numero'));
-console.log(numero_8);
+console.log('Numero 8: '+ numero_8);
 
 const numero_9 = Number(prompt('Intesisci il nono numero'));
-console.log(numero_9);
+console.log('Numero 9: '+ numero_9);
 
 const numero_10 = Number(prompt('Intesisci il cecimo e l\'ultimo numero'));
-console.log(numero_10);
+console.log('Numero 10: '+ numero_10);
 
 
 let somma = numero_1 + numero_2 + numero_3 + numero_4 + numero_5 + numero_6 + numero_7 + numero_8 + numero_9 + numero_10;
-console.log('La somma dei numeri è: ' + somma);
+console.log('La somma dei numeri: ' + somma);
