df9cd5178845bda879ef7baa7c3a3b0c2b69d771
DSlundro <dragan.savic95@outlook.it>
2022-03-30 10:24:44 +0200

    initial commit

---
+++ snack-1/main.js
@@ -0,0 +1,20 @@
+//      SNACK 1 
+//  L’utente inserisce due numeri in successione, con due prompt.
+//  Il software stampa il maggiore.
+
+
+
+const numberOne = prompt('Inserisci il primo numero');
+console.log(numberOne)
+
+const numberTwo = prompt('Inserisci il secondo numero');
+console.log(numberTwo)
+
+
+if (numberOne > numberTwo){
+    console.log('Questo è il numero maggiore ' + numberOne)
+}
+if (numberOne < numberTwo){
+    console.log('Questo è il numero maggiore ' + numberTwo)
+}
+
